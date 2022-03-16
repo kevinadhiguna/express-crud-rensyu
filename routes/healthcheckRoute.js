a5fe9a0059deb2ed083b1d82955ac1578b9ef046
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2022-03-16 14:56:57 +0700

    refactor: import and apply healthcheck controllerD

--- routes/healthcheckRoute.js
+++ routes/healthcheckRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// Import healthcheck controller
+const isServiceHealthy = require("../controllers/healthcheckController");
+
 // Check health
-router.get("/");
+router.get("/", isServiceHealthy);
 
 module.exports = router;
