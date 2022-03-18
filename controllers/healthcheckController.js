3c21c1a986a9b6db29dfd3f751473769e43c05d7
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2022-03-18 11:34:36 +0700

    refactor: format time and change duration to uptime

--- controllers/healthcheckController.js
+++ controllers/healthcheckController.js
@@ -1,8 +1,15 @@
 const isServiceHealthy = (req, res, next) => {
+  // Format time (reference : https://www.freecodecamp.org/news/javascript-date-now-how-to-get-the-current-date-in-javascript/)
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+
+  // Returns how many seconds NodeJS process is running (reference : https://nodejs.org/api/process.html#processuptime)
+  const processUptime = process.uptime();
+
   const healthyData = {
-    duration: process.uptime(),
+    uptime: `Process has been running for ${processUptime} seconds.`,
     message: "Service is healthy !",
-    time: Date.now(),
+    time: today.toUTCString(),
   };
 
   const unhealthyData = {
