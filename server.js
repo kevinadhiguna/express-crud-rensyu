558f88a5b599fb3b0896f6de8a0eb47b1835c683
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2022-03-20 15:23:22 +0700

    refactor: implement lil-http-terminator graceful shutdown

--- server.js
+++ server.js
@@ -55,6 +55,37 @@ const server = app.listen(PORT, HOSTNAME, () => {
   console.log(`App is running on ${HOSTNAME}:${PORT}, waiting for database..`);
 });
 
+const createHttpTerminator = require("lil-http-terminator");
+
+// Reference : https://github.com/flash-oss/lil-http-terminator#usage
+const httpTerminator = createHttpTerminator({
+  server,
+  // gracefulTerminationTimeout: 1000, // optional
+  // maxWaitTimeout: 30000, // optional
+});
+
+// Function triggered to shutdown server
+async function shutdown(signalOrEvent) {
+  console.log(`\n${signalOrEvent} occured, shutting down..`);
+  const { code, message, success, error } = await httpTerminator.terminate();
+  console.log(`
+    HTTP server closure result :
+      Success: ${success},
+      Code: ${code},
+      Message: ${message},
+      Error (if exists): ${error || ""}
+  `);
+  process.exit(0);
+}
+
+// Signal
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+// Events
+process.on("unhandledRejection", shutdown);
+process.on("uncaughtException", shutdown);
+
 // Handling interrupt signal (SIGINT), such as Ctrl + C
 process.on("SIGINT", () => {
   console.log("\nReceiving SIGINT, process interrupted..");
