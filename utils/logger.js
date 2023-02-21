db191e54fa9f6e32c972c1d20e0418c07246227f
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-21 13:14:57 +0700

    refactor: add another version of initializing logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,24 @@
-const { createLogger, format, transports } = require("winston");
+// -- Version 2 --
+const winston = require("winston");
 
-const logger = createLogger({
-  level: "debug",
-  format: format.json(),
-  transports: [new transports.Console()],
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    // Write all logs with importance level of `error` or less to `error.log`
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+
+    // Write all logs with importance level of `info` or less to `combined.log`
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
 });
+
+// -- Version 1 --
+// const { createLogger, format, transports } = require("winston");
+
+// const logger = createLogger({
+//   level: "debug",
+//   format: format.json(),
+//   transports: [new transports.Console()],
+// });
 
 module.exports = logger;
