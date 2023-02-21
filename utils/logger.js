7906e22301c7a6536d7ca218a0ea917798ed0299
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-21 10:12:37 +0700

    chore: remove unnecessary import

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,4 @@
-const { createLogger, format, transport, transports } = require("winston");
+const { createLogger, format, transports } = require("winston");
 
 const logger = createLogger({
   level: "debug",
