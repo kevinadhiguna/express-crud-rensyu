53d1f78b88b3dca83ca924e8c474109b790d465e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-02-23 10:58:49 +0700

    refactor: combine logs

--- utils/logger.js
+++ utils/logger.js
@@ -1,28 +1,40 @@
-// -- Version 2 --
+// -- Version 3 --
 const winston = require("winston");
+const { combine, timestamp, json } = winston.format;
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.json(),
+  format: combine(timestamp(), json()),
   transports: [
     new winston.transports.Console(),
-
-    // Write all logs with importance level of `error` or less to `error.log`
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-
-    // Write all logs with importance level of `info` or less to `combined.log`
-    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
-// If not in production env then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest })`
-if (process.env.NODE_ENV !== "production") {
-  const devFormat = new winston.transports.Console({ 
-    format: winston.format.simple() 
-  });
-  logger.add(devFormat);
-}
+// -- Version 2 --
+// const winston = require("winston");
+
+// const logger = winston.createLogger({
+//   level: process.env.LOG_LEVEL || 'info',
+//   format: winston.format.json(),
+//   transports: [
+//     new winston.transports.Console(),
+
+//     // Write all logs with importance level of `error` or less to `error.log`
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+
+//     // Write all logs with importance level of `info` or less to `combined.log`
+//     new winston.transports.File({ filename: 'combined.log' }),
+//   ],
+// });
+
+// // If not in production env then log to the `console` with the format:
+// // `${info.level}: ${info.message} JSON.stringify({ ...rest })`
+// if (process.env.NODE_ENV !== "production") {
+//   const devFormat = new winston.transports.Console({ 
+//     format: winston.format.simple() 
+//   });
+//   logger.add(devFormat);
+// }
 
 // -- Version 1 --
 // const { createLogger, format, transports } = require("winston");
